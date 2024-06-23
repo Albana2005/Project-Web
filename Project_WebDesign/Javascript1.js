d580c42ab0b56bcb5841916b52aad05d0312f9a3
Abhiramabvr <2338018077@webmail.uad.ac.id>
2024-06-23 20:55:24 +0700

    Menambahkan file proyek HTML,CSS,Javascript yang dikerjakan oleh Abhirama NIM: 2338018077

---
+++ Project_WebDesign/Javascript1.js
@@ -0,0 +1,22 @@
+function tombolbaris1(){
+    alert("Saya Sedang Belajar Javascript!");
+}
+
+function tombolbaris2(){
+    alert("Saya Sedang Belajar Javascript!");
+    alert("Ini alert ke 2 berurutan");
+}
+
+function variabel(){
+    let x = 2;
+    let y = 10;
+    const hasil = x*y;
+    alert(hasil);
+}
+
+function variabel1(){
+    let x = 20;
+    let y = 10;
+    const hasil = x*y;
+    alert(hasil);
+}
