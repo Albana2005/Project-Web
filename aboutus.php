c883bdad86e38699cef36272f32e6d9597652d3f
Abhiramabvr <147359976+Abhiramabvr@users.noreply.github.com>
2024-07-01 08:11:26 +0700

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SekolahKode</title>
+    <link rel="stylesheet"  type="text/css" href="assets/css/indexstyle.css">
+    <script src="Javascript1.js"></script>
+</head>
+<body>
+    <header>
+         <div class="logo-holder">
+            <span class="nav_image">
+                <img src="assets/img/logobaru.jpg" alt="">
+            </span>
+            <!-- <div class="logo">A</div> -->
+            <div class="logo-text">Tutorial Website</div>
+        </div>
+        <nav>
+            <ul id="menu">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="JavaScriptPage1.html">Tutorial</a>
+                </li>
+                <li>
+                    <a href="#">About us</a>
+                </li>
+                
+        </nav>
+    </header>
+    
+    <div class="new-visitor" style=" background-image: linear-gradient(rgba(139, 201, 230, 0.75),rgba(240, 191, 158, 0.75));">
+        <h2>Abhirama balaphradana Vishnu R - 2338018077</h2>
+        <p class="visitor">Bertanggung jawab mengerjakan design html, css, pada bagian menu utama website hingga about us<br> 
+            juga pengerjaan materi materi javascript untuk menu tutorial sehingga mudah dipahami oleh pengunjung, membantu pengerjaan PHP dan Javascript</p>
+        <p></p> <br>
+        
+    </div>
+    <div class="new-visitor" style=" background-image: linear-gradient(rgba(139, 201, 230, 0.75),rgba(240, 191, 158, 0.75));">
+        <h2>Albana-  </h2>
+        <p class="visitor">Bertanggung jawab mengerjakan bagian menu utama tutorial menggunakan html, css, javascript<br> 
+            juga pengerjaan materi materi css untuk menu tutorial, membuat agar tampilan tampak dinamis dan interaktif</p>
+        <p></p> <br>
+        
+    </div>
+    <div class="new-visitor" style=" background-image: linear-gradient(rgba(139, 201, 230, 0.75),rgba(240, 191, 158, 0.75));">
+        <h2>Septian Eko Nugroho - </h2>
+        <p class="visitor">Bertanggung jawab mengerjakan javascript, PHP untuk keperluan berlangganan website <br> 
+            sehingga PHP dapat digunakan sebagai forms untuk melakukan subscribe, juga membantu mengerjakan materi html</p>
+        <p></p> <br>
+        
+    </div>
+    
+    
+</body>
+</html>
